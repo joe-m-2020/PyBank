cfbe080fe0d57e05fbd8aca317b7144f502ae823
Mr Magpie <joe.marrone@fastestinc.com>
2020-10-09 21:27:18 -0500

    Months total and Profit/Loss Total

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import csv
 
 budget_csv= os.path.join('resources', 'budget_data.csv')
 
-
+# def sum(numbers):
+#     length = len(numbers)
+#     total = 0
+#     for number in numbers:
+#         total = total + number
+#         return total
+# print(sum([3,4,5]))
 # def budget_info(budget_list):
 #     Date = str(budget_list[0])
 #     PL = int(budget_list[1])
@@ -17,8 +23,24 @@ with open(budget_csv, 'r') as python_csvfile:
     csvreader = csv.reader(python_csvfile, delimiter=',')
 
     header = next(csvreader)
-     
+    
+    # since each index is a new month, the total number of lines after the header are the total months 
+    # months = 0
+    # for line in python_csvfile:
+    #     months = months + 1
 
-    for line in python_csvfile:
-        print(line)
+    # print("Total months: ", months)
 
+
+    months = 0
+    profit_loss = []
+    # totalpl = 0
+    for row in python_csvfile:
+        profit_loss.append(int(row[9:-1]))
+        months = months + 1
+    print("Total Months: ", months)
+    print("Total profit: ", sum(profit_loss))
+
+ 
+
+    
