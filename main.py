0731f2b5884866ca5f732da52f70169605ea680e
Mr Magpie <joe.marrone@fastestinc.com>
2020-10-09 23:32:22 -0500

    add gr inc. and gr. dec. NOTE Avg Ch is wrong

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import csv
 
 budget_csv= os.path.join('resources', 'budget_data.csv')
 
+#function to find average of change in profit/loss
 def average(numbers):
     length = len(numbers)
     total = 0.0
@@ -20,32 +21,52 @@ with open(budget_csv, 'r') as python_csvfile:
 
     header = next(csvreader)
     
-    # since each index is a new month, the total number of lines after the header are the total months 
-    # months = 0
-    # for line in python_csvfile:
-    #     months = months + 1
+   
 
-    # print("Total months: ", months)
+    #define variables
 
-
-    months = 0
+    
+    #lists to add columns profit loss and dates as well as change in profit loss to calculate average and min
     profit_loss = []
+    date = []
     change = []
-    first_num = 0
-    
-    
+    months = 0
+
+    #slice original data and change numbers to integer and separate dates into lists profit_loss and date
+    #the number of rows = the number of months since it is 1 row per month
+   
     for row in python_csvfile:
         profit_loss.append(int(row[9:-1]))
+        date.append(row[0:8])
         months = months + 1
 
+    
+    #subtract the first number in the iteration from the second number and pass the average function to solve
+    #for average change
     for i in range(len(profit_loss)-1):
         change.append(profit_loss[i+1]-profit_loss[i])
-        average_chage = average(change)
+        average_change = average(change)
+
+    '''''
+    !!!!!!!!!!!!average change should be -2315.12!!!!!!!!!!!!
+
+    '''''
+    
+    #find greatest increase in list "change" with min and max
+    #use the index function of the greatest increase and decrease then print the gi_increase +1 and gd_increase +1
+    #because the change list starts on row index 1 comparatively if the date and PL start on 0
+    greatest_decrease = min(change)
+    greatest_increase = max(change)
+    gd_index = change.index(greatest_decrease)
+    gi_index = change.index(greatest_increase)
 
 
-    print("Total Months: ", months)
-    print("Total profit: ", sum(profit_loss))
-    print("Average Change: ", average_chage)
- 
+    # print data summary
+    print("Financial Analysis")
+    print(f"Total Months: {months}")
+    print(f"Total profit: {sum(profit_loss)}")
+    print(f"Average Change: {average_change}")
+    print(f"Greatest Increase: {date[gi_index+1]} (${change[gi_index]})")
+    print(f"Greatest Decrease: {date[gd_index+1]} (${change[gd_index]})")
 
     
